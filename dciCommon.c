737fe6bb66527c762ce3b64564a00e2afe623824
Dawid <41573935+dbulkowski95@users.noreply.github.com>
2018-12-12 08:28:06 +0100

    Removed coment from source file and put it to header

--- dciCommon.c
+++ dciCommon.c
@@ -11,20 +11,6 @@ char* dciStrArguments[] = {"dci0", "dci1","dci60a"};
 uint8_t dciBandwidth[AMOUNT_OF_BANDWIDTHS] = {1, 3, 5, 10, 15, 20};
 uint8_t dciBandwidthPRB[AMOUNT_OF_BANDWIDTHS] = {6, 15, 25, 50, 75, 100};
 
-/* > Function: dci_DefineDci
- *******************************************************************************************/
-/*
- * @brief 	Validate arguments from argv[] and assign them to @dci_p and @prb_p. When argv[1]
- * 			is different than values in dciStrArguments function return standard dci0 type.
- * 			This same happend to argv[2] where standard bandwith size is 20 MHz and return number
- * 			of Power Resoure Block
- *
- * @param[in] argc							Amount of elements in argv[] array
- * @param[in] argv[]						Array with type of dci and bandwith
- * @param[out] dci_p						Pointer return type of dci
- * @param[out] prb_p						Pointer return nr of PRB's
- *
- *******************************************************************************************/
 void dci_defineDci(const int argc, const char* const argv[], dciType* restrict const  dci_p,
 		uint8_t* restrict const prb_p )
 {
