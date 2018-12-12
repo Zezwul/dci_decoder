470ece1841229a786f11e94b12b2a4811d77ca24
Michal <michal.jaskolski@tieto.com>
2018-12-12 10:50:50 +0100

    Created function for checking length RIV and Bitmap parameter dependent from bandwidth

--- dciCommon.c
+++ dciCommon.c
@@ -7,24 +7,111 @@
 #include <math.h>
 #include <string.h>
 
+/* dci0 Length of parameters */
+#define FIRSTPRB1 7
+#define LASTPRB1 7
+#define MCS0 5
+#define NDI0 1
+#define TPC0 2
+#define DMRS 3
+#define CSIR 1
+#define SRSR 1
+
+#define DCI0_NUMBER_PARAM 8
+#define DCI0_OFFSET_ARRAY {FIRSTPRB, LASTPRB, MCS0, NDI0, TPC0, DMRS, CSIR, SRSR};
+
+/* dci1 Length of parameters */
+#define MCS1 5
+#define HARQ1 3
+#define NDI1 1
+#define RV1 2
+#define TPC1 2
+
+#define DCI1_NUMBER_PARAM 5
+#define DCI1_OFFSET_ARRAY {MCS1, HARQ, NDI1, RV1, TPC1};
+
+/* dci60a Length of parameters */
+#define FIRSTPRB60A 7
+#define LASTPRB60A 7
+#define MCS60A 4
+#define PUSCH 2
+#define HARQ60A 3
+#define NDI60A 1
+#define RV60A 2
+#define TPC60A 2
+#define CSIR60A 1
+#define SRSR60A 1
+#define PDCCH 2
+
+#define DCI60A_NUMBER_PARAM 11
+#define DCI60A_OFFSET_ARRAY {FIRSTPRB60A, LASTPRB60A, MCS60A, PUSCH, HARQ60A, NDI60A,\
+	RV60A, TPC60A, CSIR60A, SRSR60A, PDCCH};
+
 char* dciStrArguments[] = {"dci0", "dci1","dci60a"};
+
 uint8_t dciBandwidth[AMOUNT_OF_BANDWIDTHS] = {1, 3, 5, 10, 15, 20};
 uint8_t dciBandwidthPRB[AMOUNT_OF_BANDWIDTHS] = {6, 15, 25, 50, 75, 100};
 
-/* > Function: dci_DefineDci
- *******************************************************************************************/
-/*
- * @brief 	Validate arguments from argv[] and assign them to @dci_p and @prb_p. When argv[1]
- * 			is different than values in dciStrArguments function return standard dci0 type.
- * 			This same happend to argv[2] where standard bandwith size is 20 MHz and return number
- * 			of Power Resoure Block
- *
- * @param[in] argc							Amount of elements in argv[] array
- * @param[in] argv[]						Array with type of dci and bandwith
- * @param[out] dci_p						Pointer return type of dci
- * @param[out] prb_p						Pointer return nr of PRB's
- *
- *******************************************************************************************/
+uint8_t dci0_lengthOfRIVviaBandwidth(uint8_t Bandwidth, uint8_t LengthBitsRIV, uint8_t RIV)
+{
+	uint8_t possibleLengthBitsOfRIV[AMOUNT_OF_BANDWIDTHS] = {5, 7, 7, 11, 12 ,13};
+
+	for (uint8_t i = 0; i < AMOUNT_OF_BANDWIDTHS; ++i)
+	{
+		if (Bandwidth == dciBandwidth[i])
+		{
+			LengthBitsRIV = possibleLengthBitsOfRIV[i];
+
+			if (RIV > LengthBitsRIV)
+			{
+				fprintf(stdout, "ERR_OCC_Wrong_length_bits\n");
+			}
+			return 0;
+		}
+	}
+	fprintf(stdout, "ERR_OCC_Typed_bandwidth_is_wrong");
+}
+
+uint8_t dci60a_lengthOfRIVviaBandwidth(uint8_t Bandwidth, uint8_t LengthBitsRIV, uint8_t RIV)
+{
+	uint8_t possibleLengthBitsOfRIV[AMOUNT_OF_BANDWIDTHS] = {5, 7, 7, 11, 12 ,13};
+
+	for (uint8_t i = 0; i < AMOUNT_OF_BANDWIDTHS; ++i)
+	{
+		if (Bandwidth == dciBandwidth[i])
+		{
+			LengthBitsRIV = possibleLengthBitsOfRIV[i];
+
+			if (RIV > LengthBitsRIV)
+			{
+				fprintf(stdout, "ERR_OCC_Wrong_length_bits\n");
+			}
+			return 0;
+		}
+	}
+	fprintf(stdout, "ERR_OCC_Typed_bandwidth_is_wrong");
+}
+
+uint8_t dci1_lengthOfBitmapViaBandwidth(uint8_t Bandwidth, uint8_t LengthBitsRBG, uint8_t rbgNumber)
+{
+	uint8_t possibleLengthBitsRBG[AMOUNT_OF_BANDWIDTHS] = {6, 8, 13, 17, 19 ,25};
+
+	for (uint8_t i = 0; i < AMOUNT_OF_BANDWIDTHS; ++i)
+	{
+		if (Bandwidth == dciBandwidth[i])
+		{
+			LengthBitsRBG = possibleLengthBitsRBG[i];
+
+			if (rbgNumber > LengthBitsRBG)
+			{
+				fprintf(stdout, "ERR_OCC_Wrong_length_bits\n");
+			}
+			return 0;
+		}
+	}
+	fprintf(stdout, "ERR_OCC_Typed_bandwidth_is_wrong");
+}
+
 void dci_defineDci(const int argc, const char* const argv[], dciType* restrict const  dci_p,
 		uint8_t* restrict const prb_p )
 {
--- dciCommon.h
+++ dciCommon.h
@@ -17,18 +17,59 @@ extern uint32_t* dci_tempParameters;
 extern char* dci_output;
 extern uint32_t* dci_argsToValid;
 
+
+/* > Function: dci_readValueFromDCI
+ *******************************************************************************************/
 /*
- *	 =====	dci_readValueFromDCI =====
  * Function return a pointer (uint32_t*) to array with values decode from DCI.
  * dci is our DCI read from input of programm;
  * bitLenghtOfDciParameteris array with bit lenght of every DCI parameter;
  * sizeOfArray is number of arrays element of shiftArray;
- */
+ *
+ *******************************************************************************************/
 
 uint32_t* dci_readValueFromDCI (uint64_t dci, const uint8_t bitLenghtOfDciParameter[], const uint8_t sizeOfArray);
 
+/* > Function: dci0_lengthOfRIVviaBandwidth and dci60a_lengthOfRIVviaBandwidth
+ *******************************************************************************************/
+/*
+ * @brief
+ * Checks correctness of length RIV for dci0 and dci60a.
+ * Created Bandwidth and RIV parameter.
+ * If RIV parameter is greater than possible length RIV view information
+ *
+ *******************************************************************************************/
+uint8_t dci0_lengthOfRIVviaBandwidth(uint8_t Bandwidth, uint8_t LengthBitsRIV, uint8_t RIV);
+uint8_t dci60a_lengthOfRIVviaBandwidth(uint8_t Bandwidth, uint8_t LengthBitsRIV, uint8_t RIV);
+
+/* > Function: dci1_lengthOfBitmapVviaBandwidth
+ *******************************************************************************************/
+/*
+ * @brief
+ * Checks correctness of length BitsRBG for dci1.
+ * Created Bandwidth and rbgNumber parameter.
+ * If rbgNumber parameter is greater than possible length bits RBG view information.
+ *
+ *******************************************************************************************/
+uint8_t dci1_lengthOfBitmapViaBandwidth(uint8_t Bandwidth, uint8_t LengthBitsRBG, uint8_t rbgNumber);
+
+/* > Function: dci_DefineDci
+ *******************************************************************************************/
+/*
+ * @brief 	Validate arguments from argv[] and assign them to @dci_p and @prb_p. When argv[1]
+ * 			is different than values in dciStrArguments function return standard dci0 type.
+ * 			This same happend to argv[2] where standard bandwith size is 20 MHz and return number
+ * 			of Power Resoure Block
+ *
+ * @param[in] argc							Amount of elements in argv[] array
+ * @param[in] argv[]						Array with type of dci and bandwith
+ * @param[out] dci_p						Pointer return type of dci
+ * @param[out] prb_p						Pointer return nr of PRB's
+ *
+ *******************************************************************************************/
 void dci_defineDci(const int argc, const char* const argv[], dciType* restrict const  dci_p,
 		uint8_t* restrict const prb_p);
+
 uint8_t dci_readStdin(uint64_t dci_readArgumentsStdin);
 void dci_print(char* output);
 
