5bfb3343226f08c9fae965015219f87d5fddb6a3
Aleksandra Baranska <aleksandra.baranska96@gmail.com>
2018-12-19 13:05:31 +0100

    Added debug print macro header.
    Added a macro, that after the error is found prints the error message (string)
    along with the number of the error found line and the file name.
    Added the appropriate include in the common.c and changed the printfs to the debug print calls.
    Changed the function call in main function from dci_lenghtrivviabandwidth to dci0_lenghtrivviabandwidth.

---
+++ debug.h
@@ -0,0 +1,20 @@
+#ifndef DEBUG_H_
+#define DEBUG_H_
+
+#include <string.h>
+#include <stdio.h>
+
+#define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
+
+#ifdef DEBUG
+#define DEBUG_PRINT(msg)\
+do{   \
+    (void)fprintf(stdout, "%s\t Error in line:%d, in file:%s\n", msg, __LINE__, __FILENAME__);   \
+}while(0)
+#else
+#define DEBUG_PRINT(msg)
+#endif
+
+
+
+#endif /* DEBUG_H_ */
