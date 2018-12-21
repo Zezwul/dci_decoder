ce2519c9bf5b5d40961a56831ac9bf5f0fc6e9f2
dbulkowski95 <d.bulkowski95@gmail.com>
2018-12-21 17:33:33 +0100

    Change array's name in dci0 and dci1 to correct

--- main.c
+++ main.c
@@ -17,33 +17,33 @@ int main(const int argc, const char* argv[])
 	{
 	case dci0:
 	{
-        uint32_t dci0_offsetArray[DCI0_NUMBER_PARAM] =
-        { FORMAT_FLAG, HOPPING_FLAG, RIV_LEN, MCS, NDI, TPC, DMRS, CSIR, SRSR };
-        dci0_offsetArray [dci0_rivOutput] = dci0_lengthOfRIVviaBandwidth(dci_bandwidth);
+		uint32_t dci0_offsetArray[DCI0_NUMBER_PARAM] =
+		{ FORMAT_FLAG, HOPPING_FLAG, RIV_LEN, MCS, NDI, TPC, DMRS, CSIR, SRSR };
+		dci0_offsetArray [dci0_rivOutput] = dci0_lengthOfRIVviaBandwidth(dci_bandwidth);
 
-        const uint32_t dci0_shiftOrigin = 31;
+		const uint32_t dci0_shiftOrigin = 31;
 
-        uint32_t* redValueFromDcidci = malloc(sizeof(uint32_t) * dci0_maxAmmountOfArguments);
-        redValueFromDcidci = dci_readValueFromDCI(inputArguments, dci0_offsetArray,
-                        DCI0_NUMBER_PARAM, dci0_shiftOrigin);
-        uint32_t* outputArray = malloc(sizeof(uint32_t) * dci0_maxAmountOfArgumentsOutput);
-        dci_rivDecode(dci_bandwidthPRB, redValueFromDcidci[dci0_rivOutput],
-                &outputArray[dci0_firstPRBOutput], &outputArray[dci0_lastPRBOutput]);
+		uint32_t* readValueFromDci0 = malloc(sizeof(uint32_t) * dci0_maxAmmountOfArguments);
+		readValueFromDci0 = dci_readValueFromDCI(inputArguments, dci0_offsetArray,
+				DCI0_NUMBER_PARAM, dci0_shiftOrigin);
+		uint32_t* outputArray = malloc(sizeof(uint32_t) * dci0_maxAmountOfArgumentsOutput);
+		dci_rivDecode(dci_bandwidthPRB, readValueFromDci0[dci0_rivOutput],
+				&outputArray[dci0_firstPRBOutput], &outputArray[dci0_lastPRBOutput]);
 
-        dci0_OutputParameters outputElementIndex = dci0_mcsindexOutput;
-        dci0_InputParameters inputElementIndex = dci0_mcsindex;
-        for ( ; outputElementIndex < dci0_maxAmountOfArgumentsOutput;
-                outputElementIndex++, inputElementIndex++)
-        {
-            outputArray[outputElementIndex] = redValueFromDcidci[inputElementIndex];
-        }
+		dci0_OutputParameters outputElementIndex = dci0_mcsindexOutput;
+		dci0_InputParameters inputElementIndex = dci0_mcsindex;
+		for ( ; outputElementIndex < dci0_maxAmountOfArgumentsOutput;
+				outputElementIndex++, inputElementIndex++)
+		{
+			outputArray[outputElementIndex] = readValueFromDci0[inputElementIndex];
+		}
 
-        for (dci0_OutputParameters i = dci0_firstPRBOutput;
-                i < dci0_maxAmountOfArgumentsOutput; ++i)
-        {
-            fprintf(stdout, "%u ", outputArray[i]);
-        }
-        fprintf(stdout, "\n");
+		for (dci0_OutputParameters i = dci0_firstPRBOutput;
+				i < dci0_maxAmountOfArgumentsOutput; ++i)
+		{
+			fprintf(stdout, "%u ", outputArray[i]);
+		}
+		fprintf(stdout, "\n");
 		break;
 	}
 	case dci1:
@@ -52,23 +52,23 @@ int main(const int argc, const char* argv[])
 		dci1_offsetArray[dci1_bitmap] = dci1_lengthOfBitmapViaBandwidth(dci_bandwidth);
 		uint32_t dci1_shiftOrigin = dci1_calculateShiftOrigin(dci1_offsetArray);
 
-		uint32_t* redValueFromDci1 = dci_readValueFromDCI(inputArguments, dci1_offsetArray,
+		uint32_t* readValueFromDci1 = dci_readValueFromDCI(inputArguments, dci1_offsetArray,
 				DCI1_NUMBER_PARAM, dci1_shiftOrigin);
 
-		uint32_t* outputRBGIndex =  dci1_bitmapDecoder(redValueFromDci1[dci1_bitmap], dci1_offsetArray[dci1_bitmap]);
+		uint32_t* outputRBGIndex =  dci1_bitmapDecoder(readValueFromDci1[dci1_bitmap], dci1_offsetArray[dci1_bitmap]);
 
-		dci1_printResults(redValueFromDci1, outputRBGIndex);
+		dci1_printResults(readValueFromDci1, outputRBGIndex);
 		break;
 	}
 	case dci60a:
 	{
-	    uint32_t dci60a_offsetArray[DCI60A_NUMBER_PARAM] = { RIV_LEN, MCS60A, PUSCH, HARQ, NDI, RV,
-	            TPC, CSIR, SRSR, PDCCH };
-      const uint32_t dci60a_shiftOrigin = 31;
+		uint32_t dci60a_offsetArray[DCI60A_NUMBER_PARAM] = { RIV_LEN, MCS60A, PUSCH, HARQ, NDI, RV,
+				TPC, CSIR, SRSR, PDCCH };
+		const uint32_t dci60a_shiftOrigin = 31;
 		break;
 	}
 	default:
 		break;
 	}
-return 0;
+	return 0;
 }
